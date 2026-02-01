b76a2d76e8e883ee16cca0ead4761edce7a2c79e
Shashamisen <shashamisen@gmail.com>
2026-02-01 13:43:05 +0100

    Update Wwise_IDs.h

--- SuperPasNette/SuperPasNette_WwiseProject/GeneratedSoundBanks/Wwise_IDs.h
+++ SuperPasNette/SuperPasNette_WwiseProject/GeneratedSoundBanks/Wwise_IDs.h
@@ -13,13 +13,31 @@ namespace AK
 {
     namespace EVENTS
     {
+        static const AkUniqueID AURA_ROAMER = 3931301993U;
+        static const AkUniqueID AURA_X = 2330413577U;
         static const AkUniqueID BUTTON = 977454165U;
         static const AkUniqueID BUTTONPAUSE = 3982365877U;
         static const AkUniqueID BUTTONPLAY = 2216827983U;
-        static const AkUniqueID CAMERA_ACTIVATION = 2159338999U;
+        static const AkUniqueID CAMERA_01 = 592021766U;
+        static const AkUniqueID CAMERA_02 = 592021765U;
+        static const AkUniqueID CAMERA_03 = 592021764U;
+        static const AkUniqueID CAMERA_04 = 592021763U;
+        static const AkUniqueID CAMERA_05 = 592021762U;
+        static const AkUniqueID CAMERA_MULTI = 1444493102U;
+        static const AkUniqueID CAMERA_TOP = 3290080636U;
         static const AkUniqueID FOOTSTEPS_MC = 2093862925U;
-        static const AkUniqueID PLAY_FOOTSTEPS_MCSLOWED = 4086908450U;
+        static const AkUniqueID FOOTSTEPS_MRX = 2482936940U;
+        static const AkUniqueID FOOTSTEPS_ROAMERS = 347571886U;
+        static const AkUniqueID GETTINGHURT = 876350284U;
+        static const AkUniqueID HALLUCINATIONS = 2491135207U;
+        static const AkUniqueID LIGHTSBUG = 636638068U;
+        static const AkUniqueID MARKET = 1313304937U;
+        static const AkUniqueID PICKUP = 3978245845U;
+        static const AkUniqueID PLAY_MUSICDEATHINSANITY = 13378000U;
+        static const AkUniqueID PLAY_MUSICDEATHMRX = 632994870U;
         static const AkUniqueID PLAY_MUSICGROCERYSTORE1 = 316151928U;
+        static const AkUniqueID PLAY_MUSICTITLE = 562806075U;
+        static const AkUniqueID PLAY_MUSICVICTORY = 2922853999U;
     } // namespace EVENTS
 
     namespace STATES
@@ -56,17 +74,22 @@ namespace AK
             } // namespace STATE
         } // namespace MENTALSTATE
 
-        namespace SPEED
+        namespace PARANORMAL
         {
-            static const AkUniqueID GROUP = 640949982U;
+            static const AkUniqueID GROUP = 2887363794U;
 
             namespace STATE
             {
+                static const AkUniqueID CUSTOMERS_01 = 2198517790U;
+                static const AkUniqueID LAYOUT_04 = 3442077614U;
+                static const AkUniqueID LIGHTS_03 = 3865111338U;
+                static const AkUniqueID MRX_05 = 1736576286U;
                 static const AkUniqueID NONE = 748895195U;
-                static const AkUniqueID SPEEDHIGH = 809829910U;
-                static const AkUniqueID SPEEDLOW = 1951840192U;
+                static const AkUniqueID NORMAL_00 = 187901679U;
+                static const AkUniqueID ROAMERS_02 = 4033003399U;
+                static const AkUniqueID SLOWED_06 = 2460914622U;
             } // namespace STATE
-        } // namespace SPEED
+        } // namespace PARANORMAL
 
     } // namespace STATES
 
